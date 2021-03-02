69557f9d9f614672e96222eeb9c6d2677de990e6
th28 <tomholt373@gmail.com>
2021-03-03 00:31:56 +0200

    Test commit

--- app/src/main/java/com/example/kivadict/MainActivity.java
+++ app/src/main/java/com/example/kivadict/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //MainActivity. Just holds fragments.
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
